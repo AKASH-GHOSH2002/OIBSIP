3e7d7611991ff1598be1935c48ecdac555c7e315
Akash ghosh <coderakash9635@gmail.com>
2024-06-29 06:54:52 +0530

    first commit

---
+++ LEVEL 2 TASK 4 LOGIN AUTHENTIACATION/login.js
@@ -0,0 +1,26 @@
+function auth(event) {
+    event.preventDefault();
+
+    var email = document.getElementById("gmail").value;
+    var password = document.getElementById("passkey").value;
+
+    if (email === "admin@gmail.com" && password === "user") {
+        window.location.replace("secure_page.html");
+    } else {
+        alert("Invalid information");
+        return;
+    }
+}
+
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("passkey");
+
+eyeicon.onclick = function(){
+    if(password.type == "password"){
+        password.type = "text";
+        
+    }else{
+        password.type = "password";
+        
+    }
+}
